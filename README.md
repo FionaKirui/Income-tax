e93284f5a99e866a4f4b796acb2d7e903780496a
SophiaNM <njerimurage@outlook.com>
2016-12-10 08:17:15 +0300

    changes to the readme and to the javascript logic

--- README.md
+++ README.md
@@ -1,22 +1,60 @@
-# ping-pong
-This is an  assignment on object-oriented programming.
-It tests the understanding of  necessary tools such as if-/else-statements and loops.
-if the number is divisible by 3 print ping;
-if the number is divisible by 5 print pong;
-if the number is divisible by 3 and 5 print ping-pong;
-else, print the number.
+# Income Tax Rates in Kenya(old and new rates)
+----------------------------------------------------------------------------------------------------
+This is a basic Income tax calculator with functionality allowng you to calculate your monthly taxes using the old rates and new rates to be effected on January 2017.
+The most basic variables are considered to calculate this income tax and they include: 
+ - PAYE
+ - NSSF
+ - NHIF
+ - GrossIncome
 
-## Code Example
-9 is divisible by 3 hence ping;
-10 is divisible by 5 hence pong;
-15 is divisible by 3 and 5 hence ping-pong;
-19 is not divisible by either number hence, print the number;
+# BDD
+The Specifications using old nssf rates and variables to consider include:
 
+| Behavior | Input Example | Output Example(old nssf rates) | Output Example(new nssf rates) |
+| ---------| ------------- | ------------------------------ | ------------------------------ |
+| nssf is a constant of 200 for anyone earning | 53000 | 200 | 1080 |
+| nhif is a value based on your gross income bracket | 53000 | 1200 | 1200 |
+| personal relief is a constant for anyone liaaable to pay taxes | 53000 | 1162  | 1162 |
+| taxable pay is a function of grossIncome less  personal relief | 53000 | 52800 | 51920 |
+| paye is the taxable pay less personal relief | 53000 | 9,772 | 9508.10 |
+| netpaye is a function of nhif, nssf,paye and grossIncome | 53000 | 41,828 | 411211 |
+| effective tax rate is a function of paye and grossIncome | 53000 | 18.44% | 17.94%
 
-## Motivation
-This was an object-oriented example at Moringa School.
+## Features
+It is built using HTML and CSS; Bootstrap; JavaScript and jQuery.
 
+## Live Website 
+View the complete site [here]()
+
+## Contributors
+ - Fiona Kirui
+ - Sophia Murage	
 
 ## License
+---------
+### MIT License
 
-All rights belong to Moringa School.
+Copyright (c) [2016] [Sophia Njeri Murage & Fiona Kirui]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+### Contact Information
+Sophia -
+Fiona -
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,91 +1,106 @@
 
-var taxableI= grossIncome-400
-function paye (taxableI){
-  var paye=0
-  // check the == sign
-if (taxableI<===10164){
-  paye=0.1*taxableI;
+var grossIncome;
+var taxableIncome;
+var netPay;
+var effectiveRate;
+var paye;
+var nhif;
 
-}
-else if (taxableI >===10165 && <===19740) {
-  paye=0.15*taxableI;
+var oldNssf = 200;
+var personalRelief = 1162;
 
-}
-else if (taxableI >===19741 && <===29316) {
-  paye=0.20*taxableI;
+var payeF = function (taxableIncome) {
+    var paye = 0;
+    
+    if (taxableIncome <= 10164) {
+        return paye = 0.1*taxableIncome-personalRelief;
+    }   
+        else if (taxableIncome >= 10165 && taxableIncome <= 19740) {
+        return paye = (taxableIncome-10165)*0.15 + 1016.4-personalRelief ;
+    }   
+        else if (taxableIncome >= 19741 && taxableIncome <= 29316) {
+        return paye = (taxableIncome-19741)*0.20 + 2452.65-personalRelief ;
+    }   
+        else if (taxableIncome >= 29317 && taxableIncome <= 38892) {
+        return paye = (taxableIncome-29317)*0.25 + 4367.65 - personalRelief;
+    }   
+        else if (taxableIncome >= 38892) {
+        return paye = (taxableIncome-38892)*0.3 + 6761.4-personalRelief;
+    }   
+        else return(0);
+};
 
-}
-else if (taxableI >===29317 && <===38892) {
-  paye=0.25*taxableI;
 
-}
-else if (taxableI >===38892) {
-  paye=0.3
-  *taxableI;
+var nhifF = function (grossIncome){
+    var nhif =0
 
-}
-else return(0);
+    if (grossIncome<=5999){
+        return nhif=150;
+    }   else if (grossIncome >= 6000 && grossIncome <= 7999){
+        return nhif=300;
+    }   else if (grossIncome >= 8000 && grossIncome <=11999) {
+        return nhif=400;
+    }   else if (grossIncome >= 12000 && grossIncome <=14999) {
+        return nhif=500;
+    }   else if (grossIncome >= 15000 && grossIncome <=19999) {
+        return nhif=600;
+    }   else if (grossIncome >= 20000 && grossIncome<= 24999) {
+        return nhif=750;
+    }   else if (grossIncome >= 25000 && grossIncome <= 29999) { 
+        return nhif=850;
+    }   else if (grossIncome >= 30000 && grossIncome <= 34999) {
+        return nhif=900;
+    }   else if (grossIncome >= 35000 && grossIncome <= 39999) {
+        return nhif=950;
+    }   else if (grossIncome >= 40000 && grossIncome <= 44999) {
+        return nhif=1000;
+    }   else if (grossIncome >= 45000 && grossIncome <= 49999) {
+        return nhif=1100;
+    }   else if (grossIncome >= 50000 && grossIncome <= 59999) {
+        return nhif=1200;
+    }   else if (grossIncome >= 60000 && grossIncome <= 69999) {
+        return nhif=1300;
+    }   else if (grossIncome >= 70000 && grossIncome <= 79999) {
+        return nhif=1400;
+    }   else if (grossIncome >= 80000 && grossIncome <= 89999) {
+        return nhif=1500;
+    }   else if (grossIncome >= 90000 && grossIncome <= 99999) {
+        return nhif=1600;
+    }   else if (grossIncome >= 100000) {
+        return nhif=1700;
+    }   else return(0);
+};
 
-}
+var nssfF = function () {
+    var nssf = 0;
+    if ( grossIncome <= 6000){
+        return nssf = 0.6 * grossIncome;
+    }   else if (grossIncome >= 6001 && grossIncome <= 18000){
+        return nssf = 0.6* grossIncome;
+    }   else return(1080);
+};
 
-// PAYE ends here...
-}
-function nhif(grossIncome){
-  var nhif =0
-  // check the == sign
-if (grossIncome<===5999){
-  nhif=150;
 
-}
-else if (grossIncome >===6000 && <===7999) {
-  nhif=300;
 
-}
-else if (grossIncome >===8000 && <===11999) {
-  nhif=400;
-}
-else if (grossIncome >===12000 && <===14999) {
-  nhif=500;
-}
-else if (grossIncome >===15000 && <===19999) {
-  nhif=600;
-}
-else if (grossIncome >===20000 && <===24999) {
-  nhif=750;
-}
-else if (grossIncome >===25000 && <===29999) {
-  nhif=850;
-}
-else if (grossIncome >===30000 && <===34999) {
-  nhif=900;
-}
-else if (grossIncome >===35000 && <===39999) {
-  nhif=950;
-}
-else if (grossIncome >===40000 && <===44999) {
-  nhif=1000;
-}
-else if (grossIncome >===45000 && <===49999) {
-  nhif=1100;
-}
-else if (grossIncome >===50000 && <===59999) {
-  nhif=1200;
-}
-else if (grossIncome >===60000 && <===69999) {
-  nhif=1300;
-}
-else if (grossIncome >===70000 && <===79999) {
-  nhif=1400;
-}
-else if (grossIncome >===80000 && <===89999) {
-  nhif=1500;
-}
-else if (grossIncome >===90000 && <===99999) {
-  nhif=1600;
-}
-else if (grossIncome >===100000) {
-  nhif=1700;
-}
+$(document).ready(function() {
+  $("#incomeTax").submit(function(event){
+        var grossIncome = parseInt($("input#income").val());
+        var taxableIncome = grossIncome - nssfF();
+        var netPay = grossIncome - nssfF() - payeF() - nhifF();
+        var effectiveRate = payeF(taxableIncome) / grossIncome * 100;
 
-}
-// nhif ends here
+        event.preventDefault();
+        
+
+        alert(grossIncome);
+        alert(nssfF());
+        alert(taxableIncome);
+        alert(personalRelief);
+        alert(payeF(taxableIncome));
+        alert(nhifF(grossIncome));
+        alert(netPay);
+        alert(effectiveRate);
+      
+  });
+
+});
